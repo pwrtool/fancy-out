ea1a45e7fb983791d9d896c9435e10e3a0b7b76d
firesquid <jdeiss06@gmail.com>
2023-10-09 20:54:25 -0500

    add fancyout class

--- index.ts
+++ index.ts
@@ -1,1 +1,55 @@
-console.log("Hello via Bun!");
+export class FancyOut {
+  /**
+   * Outputs a message to the console
+   * @param message - The message to output
+   * @return void
+   */
+  static out(message: string) {
+    console.log(message);
+  }
+
+  /**
+   * Outputs a message to the console in green and bold text
+   * @param message - The message to output
+   * @return void
+   */
+  static success(message: string) {
+    console.log("\x1b[32;1m%s\x1b[0m", message);
+  }
+
+  /**
+   * Outputs a message to the console in blue and bold text
+   * @param message - The message to output
+   * @return void
+   */
+  static bold(message: string) {
+    console.log("\x1b[37;1m%s\x1b[0m", message);
+  }
+
+  /**
+   * Outputs a message to the console in yellow and bold text
+   * @param message - The message to output
+   * @return void
+   */
+  static warn(message: string) {
+    console.log("\x1b[33;1m%s\x1b[0m", message);
+  }
+
+  /**
+   * Outputs a message to the console in bold blue text with an underline
+   * @param message - The message to output
+   * @return void
+   */
+  static header(message: string) {
+    console.log("\x1b[34;1;4m%s\x1b[0m", message);
+  }
+
+  /**
+   * Outputs a message to the console in red and bold text
+   * @param message - The message to output
+   * @return void
+   */
+  static error(message: string) {
+    console.log("\x1b[31;1m%s\x1b[0m", message);
+  }
+}
